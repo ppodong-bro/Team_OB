570864019e33aad157e94346dfb86b58dc0de3e4
AhriIsLove <60705922+AhriIsLove@users.noreply.github.com>
2025-07-22 15:35:10 +0900

    Init Package
    시작

---
+++ AssemERP/src/test/java/com/WiseForce/AssemERP/AssemErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WiseForce.AssemERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
