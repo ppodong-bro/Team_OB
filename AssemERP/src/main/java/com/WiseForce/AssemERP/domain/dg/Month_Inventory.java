ddb633082cb976a7450a47dddc7b0c1d319df43b
AhriIsLove <60705922+AhriIsLove@users.noreply.github.com>
2025-07-25 16:22:11 +0900

    domain ADD inventory
    재고 엔티티 추가

---
+++ AssemERP/src/main/java/com/WiseForce/AssemERP/domain/dg/Month_Inventory.java
@@ -0,0 +1,27 @@
+package com.WiseForce.AssemERP.domain.dg;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Month_Inventory {
+	@EmbeddedId
+	Month_Inventory_ID month_Inventory_ID;
+	// 년월
+	// 기초/기말 구분
+	// 제품/부품 구분
+	// 제품/부품번호
+
+	int cnt;// 수량
+	LocalDateTime in_date;// 등록일자
+}
