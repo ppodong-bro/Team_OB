58536147215e4b3bede43882c06a7f5f6e1d19f4
ppodong-bro <msn1222ysh@gmail.com>
2025-07-29 10:19:31 +0900

    apply  annotation
    apply  annotation

--- AssemERP/src/main/java/com/WiseForce/AssemERP/controller/dg/InventoryController.java
+++ AssemERP/src/main/java/com/WiseForce/AssemERP/controller/dg/InventoryController.java
@@ -8,32 +8,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import com.WiseForce.AssemERP.domain.dg.Month_Inventory;
-import com.WiseForce.AssemERP.dto.dg.Month_InventoryDTO;
-import com.WiseForce.AssemERP.service.dg.InventoryService;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class InventoryController {
-	private final InventoryService inventoryService;
-
-	@GetMapping("/inventory")
-	public String inventory(Model model) {
-		// 현재 재고 목록 조회
-		List<Month_InventoryDTO> month_Inventories = inventoryService.getCurrentList();
-
-		model.addAttribute("month_Inventories", month_Inventories);
-
-		// 인벤토리 화면 이동
-		return "dg/inventoryList";
-	}
-
-	@PutMapping("/inventory/monthClose")
-	public String monthClose() {
-		System.out.println("월마감 시작합니다.");
-		
-		return "";
-	}
+	/*
+	 * private final InventoryService inventoryService;
+	 * 
+	 * @GetMapping("/inventory") public String inventory(Model model) { // 현재 재고 목록
+	 * 조회 List<Month_InventoryDTO> month_Inventories =
+	 * inventoryService.getCurrentList();
+	 * 
+	 * model.addAttribute("month_Inventories", month_Inventories);
+	 * 
+	 * // 인벤토리 화면 이동 return "dg/inventoryList"; }
+	 * 
+	 * @PutMapping("/inventory/monthClose") public String monthClose() {
+	 * System.out.println("월마감 시작합니다.");
+	 * 
+	 * return ""; }
+	 */
 
 }
