749eab5c99832e3ee9ef862b2070c49d12999a0b
Lee-soon-mi <grobalgrowseo@gmail.com>
2025-07-30 14:08:25 +0900

    EmpDTO Upload
    EmpDTO Upload(타파트 공유목적)

---
+++ AssemERP/src/main/java/com/WiseForce/AssemERP/dto/sm/EmpDTO.java
@@ -0,0 +1,26 @@
+package com.oracle.AssemERP.dto.sm;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class EmpDTO 
+{
+	// EMP 테이블
+	private int 	empNo;
+	private String 	empName;
+	private String 	empTel;
+	private String 	email;
+	private long 	sal;
+	private int 	deptCode;
+	private String 	username;
+	private String 	password;	 // 평문 비밀번호를 임시 저장
+	private String 	rolesStatus; // "ROLE_ADMIN,ROLE_USER" 형태
+	private int 	delStatus;
+	private int 	registrar;
+	private Date 	inDate;
+    
+    // EMP_IMAGE 테이블 (대표 이미지 1개)
+    private String empFilename;
+}
