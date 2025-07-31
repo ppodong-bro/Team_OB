f62b24d6aabdf0a895eeed0701e5e9695df562d9
AhriIsLove <60705922+AhriIsLove@users.noreply.github.com>
2025-07-31 12:27:58 +0900

    inventoryList Search Add
    재고 조회 기능 추가

--- AssemERP/src/main/java/com/WiseForce/AssemERP/controller/dg/InventoryController.java
+++ AssemERP/src/main/java/com/WiseForce/AssemERP/controller/dg/InventoryController.java
@@ -24,8 +24,10 @@ public class InventoryController {
 
 	@GetMapping("/inventory")
 	public String inventory(Real_InventoryDTO real_InventoryDTO, Model model) {
+//		System.out.println(real_InventoryDTO);
+		
 		// 현재 재고의 종류 수 조회
-		int totalTypeCount = inventoryService.getTotalTypeCount(real_InventoryDTO.getItem_type());
+		int totalTypeCount = inventoryService.getTotalTypeCount(real_InventoryDTO);
 		
 		// 전체 개수와 요청한 현재 페이지를 토대로 start와 end를 설정한다.
 		Paging page = new Paging(totalTypeCount, real_InventoryDTO.getCurrentPage());
@@ -34,14 +36,15 @@ public class InventoryController {
 
 		// 설정한 start, end로 현재 재고 전체 조회
 		List<Real_InventoryDTO> real_InventoryDTOs = inventoryService.getRealInventory(real_InventoryDTO);
-		
+
+		model.addAttribute("search", real_InventoryDTO);
 		model.addAttribute("realInventoryList", real_InventoryDTOs);
 		model.addAttribute("page", page);
 		
 		// 인벤토리 화면 이동
 		return "dg/inventoryList";
 	}
-
+	
 	@PutMapping("/inventory/monthClose")
 	public String monthClose() {
 		System.out.println("월마감 시작합니다.");
